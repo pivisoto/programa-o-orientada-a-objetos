5d44411e7cd7f4afa9719279a445fef720891492
pivisoto <103545280+pivisoto@users.noreply.github.com>
2022-04-13 07:25:50 -0300

    Add files via upload

---
+++ ContadorInverso.java
@@ -0,0 +1,10 @@
+
+public class ContadorInverso{
+    public static void main(String[] args) {
+        int contador = 100;
+        do {
+            System.out.println(contador);
+            contador = contador - 1;
+        }while (contador != -1);
+    }
+}
