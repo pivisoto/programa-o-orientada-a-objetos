fdd97cce7d4d5d23082485791c9fa9c07d808339
pivisoto <103545280+pivisoto@users.noreply.github.com>
2022-04-13 08:02:13 -0300

    Add files via upload

---
+++ FunçãoDoi.java
@@ -0,0 +1,16 @@
+
+public class Teste {
+
+    public static void main(String[] args) {
+
+        int i = 0;
+        System.out.println(i);
+        i++;
+        System.out.println(i);
+        ++i;
+        System.out.println(i);
+        i = i + 1;
+        System.out.println(i);
+
+    }
+}
